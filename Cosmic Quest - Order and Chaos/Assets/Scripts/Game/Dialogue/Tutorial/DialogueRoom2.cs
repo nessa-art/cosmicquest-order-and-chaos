d002f88c4f68e1ab342a09b84cfb889ec265e803
nessa-art <nessenceart@gmail.com>
2019-11-23 20:58:13 -0500

    Adding lvl 1 room 1 level design.

---
+++ Cosmic Quest - Order and Chaos/Assets/Scripts/Game/Dialogue/Tutorial/DialogueRoom2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueRoom2 : MonoBehaviour
+{
+    Animator anim; // Leol animation
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        anim.SetTrigger("EnterRoom");
+    }
+
+}
---
+++ Cosmic Quest - Order and Chaos/Assets/Scripts/World/ChaosVoid1/Room1/Lvl1Room1.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lvl1Room1 : Room
+{
+    public List<int> m_LeverOrder;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.ArePlatformsActivated())
+        {
+            Debug.Log("All platforms activated - Show hint");
+            Anim.SetBool("IsRevealed", true);
+        }
+    }
+
+    // Overriden: Returns whether levers have been pulled in the correct order
+    public override bool AreLeversPulled()
+    {
+        bool leversPulled = true;
+
+        if (m_Levers == null || m_Levers.Length == 0) return true;
+
+        // Check if every lever has been activated
+        foreach (GameObject lever in m_Levers)
+        {
+            Transform handle = lever.transform.Find("Handle");
+
+            if (!handle.GetComponent<Animator>().GetBool("LeverPulled"))
+            {
+                // If at least 1 lever isn't activated, return false
+                leversPulled = false;
+            }
+        }
+
+        return leversPulled;
+    }
+}
