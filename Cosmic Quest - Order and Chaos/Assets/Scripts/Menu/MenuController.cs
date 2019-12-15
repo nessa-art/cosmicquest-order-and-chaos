5e0e7e76bd43c16e4776d52fe81ba9a1e0c77c1c
nessa-art <nessenceart@gmail.com>
2019-12-15 03:49:35 -0500

    Adding audio and basic layout for each selection page menu

---
+++ Cosmic Quest - Order and Chaos/Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void StartTutorial()
+    {
+        // TODO: In Build settings, ensure Tutorial.unity is indexed to 1
+        Debug.Log("Set Tutorial.unity to index 1 in build settings");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void StartLevel1() 
+    {
+        // TODO: In Build settings, ensure ChaosVoid1.unity is indexed to 2
+        Debug.Log("Set Tutorial.unity to index 2 in build settings");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
+
