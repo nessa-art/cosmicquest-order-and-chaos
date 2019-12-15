28abac3ebfffb31acc3e54142b5473c4e4df504e
nessa-art <nessenceart@gmail.com>
2019-12-15 15:53:42 -0500

    Prototype menu for 'New' selection complete. Need to polish up character and class images and descriptions

---
+++ Cosmic Quest - Order and Chaos/Assets/Scripts/Menu/DisplayDescriptionUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+// TODO: Implement conotroller support for the menu
+public class DisplayDescriptionUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private GameObject selectionText; // Display description of highligthed selection when mouse hovers over screen
+    
+    void Start()
+    {
+        selectionText = transform.Find("Description").gameObject;
+    }
+
+    // Mouse Support
+    // Enable backstory text when character is highlighted  
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        selectionText.SetActive(true);
+    }   
+
+    // Disable backstory text when character is not highlighted anymore
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        selectionText.SetActive(false);
+    }   
+}
