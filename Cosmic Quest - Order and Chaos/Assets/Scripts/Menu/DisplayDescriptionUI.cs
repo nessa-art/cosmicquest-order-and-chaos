c8c60a3757db8c1117cbf3b829bcf9409e43cdeb
nessa-art <nessenceart@gmail.com>
2019-12-25 17:14:07 -0500

    Async load scenes, chagne selectionText to Description

--- Cosmic Quest - Order and Chaos/Assets/Scripts/Menu/DisplayDescriptionUI.cs
+++ Cosmic Quest - Order and Chaos/Assets/Scripts/Menu/DisplayDescriptionUI.cs
@@ -7,7 +7,7 @@ using UnityEngine.EventSystems;
 // TODO: Implement conotroller support for the menu
 public class DisplayDescriptionUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public GameObject selectionText; // Display description of highligthed selection when mouse hovers over screen
+    public GameObject Description; // Display description of highligthed selection when mouse hovers over screen
 
     // Mouse Support
     // Enable backstory text when character is highlighted  
