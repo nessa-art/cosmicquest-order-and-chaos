c7a0b3088c6cec8f8d2ecc82b33e980e5735dba7
nessa-art <nessenceart@gmail.com>
2019-12-25 17:12:49 -0500

    Manually assign player scripts before compilation instead of at Start()

--- Cosmic Quest - Order and Chaos/Assets/Scripts/Menu/MenuController.cs
+++ Cosmic Quest - Order and Chaos/Assets/Scripts/Menu/MenuController.cs
@@ -8,19 +8,35 @@ public class MenuController : MonoBehaviour
     public void StartTutorial()
     {
         // Load Tutorial scene
-        SceneManager.LoadScene("Tutorial", LoadSceneMode.Single);
+        StartCoroutine(LoadYourAsyncScene("Tutorial"));
     }
 
     public void StartLevel() 
     {
         // Load Level 1
-        SceneManager.LoadScene("ChaosVoid1", LoadSceneMode.Single);
+        StartCoroutine(LoadYourAsyncScene("ChaosVoid1"));
     }
 
     public void ExitGame()
     {
         Debug.Log("Exit");
         Application.Quit();
+    }
+
+    private IEnumerator LoadYourAsyncScene(string sceneName)
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
     }
 }
 
